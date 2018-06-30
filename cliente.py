39b455538362862d6ce835400e72d63ac1e21b8f
Diego Martins Gomes Saraiva <38090475+Idiego2@users.noreply.github.com>
2018-06-30 02:30:52 -0300

    Add files via upload

--- cliente.py
+++ cliente.py
@@ -1,14 +1,17 @@
-from conexao import Conexao
+import socket
+HOST = '192.168.0.115'     									#Endereco do Servidor(IP ou DNS)
+PORT = 5007            									#Porta em que a aplicacao estara "escutando"
 
-cliente = Conexao("localhost",5000)
-cliente.cliente()
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #cria um socket para conectar o cliente e o servidor
+dest = (HOST, PORT)
+tcp.connect(dest)										#realiza a conexao com o servidor
+														#esta aplicacao enviara mensagens ao servidor no momento que enviar
+														#o caractere CTRL+X(\x18 em hexadecimal) ou 
+														#parar a aplicacao o servidor ira desconectar o cliente
 
-while True:
-    msg = raw_input("Digite uma msg: ")
-    cliente.enviar(msg)
-    resposta = cliente.receber()
-    if "sair" in msg:
-        break
-    print resposta
-
-cliente.fechar()
+print 'Para sair use CTRL+X\n'
+msg = raw_input() 										#responsavel por receber a mensagem
+while msg <> '\x18':
+    tcp.send (msg)	
+    msg = raw_input()									#responsavel por receber a mensagem
+tcp.close()												#fecha a conexao
--- servidor.py
+++ servidor.py
@@ -1,20 +1,32 @@
-from conexao import Conexao
+# -*- coding: utf-8 -*-
+import socket
 import serial
-arduino = serial.Serial('COM7',9600)
-servidor = Conexao("",5000)
-servidor.servidor()
-servidor.aceitarConexao()
+arduino = serial.Serial('COM5',9600)
+HOST = ''              					# Endereco IP do Servidor
+PORT = 5003         					# Porta que o Servidor esta
 
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #cria um socket para conectar o cliente e o servidor
+orig = (HOST, PORT)
+tcp.bind(orig)					        #coloca o endereço local, o IP e a porta no socket
+tcp.listen(1)						#esta função faz com que o servidor fique no modo passivo, esperando conexões
 while True:
-    msg=servidor.receber()
-    arduino.write(msg)
-    # resp = (arduino.readline().strip())
-    #dados = resp.decode('utf-8')
-    #print dados
-    if "sair" in msg:
-        servidor.enviar("ate mais")
-        break
-    servidor.enviar(msg)
+    con, cliente = tcp.accept()				#aceita uma nova conexão
+    print 'Concetado: ', cliente			#informa o IP do cliente
+    while True:					        #neste while obriga o servidor a ficar no loop enquanto houver dados para receber
+        msg = con.recv(1024)
+        if not msg:
+            break
 
-print 'Finalizando conexao do cliente'
-servidor.fechar()
+        print cliente, msg
+        arduino.write(msg)
+       # resp = (arduino.readline().strip())
+       # dados = resp.decode('utf-8')
+    
+        
+        
+    print 'Finalizando conexao: ', cliente
+
+
+
+    
+    con.close()
